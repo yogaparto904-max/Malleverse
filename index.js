a9f88530dea054cbbf06d72004829b41ae3130e1
yogaparto904-max <yogaparto904@gmail.com>
2026-06-13 18:01:23 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot aktif!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Web server jalan");
+});
+
+const {
+  Client,
+  GatewayIntentBits
+} = require("discord.js");
+
+const {
+  joinVoiceChannel,
+  entersState,
+  VoiceConnectionStatus
+} = require("@discordjs/voice");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+const GUILD_ID = "489687951253700619";
+const VOICE_CHANNEL_ID = "1514977355096002780";
+
+let connection;
+
+async function joinVC() {
+  try {
+    const guild = client.guilds.cache.get(GUILD_ID);
+
+    if (!guild) {
+      return console.log("Guild tidak ditemukan");
+    }
+
+    const channel = guild.channels.cache.get(VOICE_CHANNEL_ID);
+
+    if (!channel) {
+      return console.log("Voice channel tidak ditemukan");
+    }
+
+    connection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: guild.id,
+      adapterCreator: guild.voiceAdapterCreator,
+      selfDeaf: true,
+      selfMute: false
+    });
+
+    console.log(`Bot masuk ke VC: ${channel.name}`);
+
+    connection.on(VoiceConnectionStatus.Disconnected, async () => {
+      console.log("Bot disconnect, mencoba reconnect...");
+
+      try {
+        await entersState(
+          connection,
+          VoiceConnectionStatus.Signalling,
+          5000
+        );
+      } catch (err) {
+        console.log("Reconnect gagal, mencoba join ulang...");
+
+        setTimeout(() => {
+          joinVC();
+        }, 5000);
+      }
+    });
+
+  } catch (err) {
+    console.log("Error join VC:");
+    console.log(err);
+  }
+}
+
+client.once("ready", async () => {
+  console.log(`${client.user.tag} online!`);
+
+  joinVC();
+});
+
+client.login(process.env.TOKEN);
